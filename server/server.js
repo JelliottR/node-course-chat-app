38c5edeacd318b1ad70d84ac03a240d5176b73f2
Elliott <elliottrecord@gmail.com>
2018-09-01 21:55:59 +0100

    Add Users class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -5,27 +5,39 @@ const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+let users = new Users();
 
 io.on('connection', (socket) => {
 
-    socket.on('disconnect', (reason) => {
-        console.log('Client disconnected...', reason);
+    socket.on('disconnect', () => {
+        const user = users.removeUser(socket.id);
+
+        if (user){
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+        }
     });
-
-    socket.emit('newMessage', generateMessage("Admin", "Welcome to the chat app!"));
-
-    socket.broadcast.emit('newMessage', generateMessage("Admin", "New user joined..."));
 
     socket.on('join', (params, callback) => {
         if (!isRealString(params.name) || !isRealString(params.room)) {
-            callback('Name and room name are required.')
+            return callback('Name and room name are required.')
         }
+
+        socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
+
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+
+        socket.emit('newMessage', generateMessage("Admin", "Welcome to the chat app!"));
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage("Admin", `${params.name} has joined.`));
 
         callback();
     });
@@ -37,7 +49,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createLocationMessage', (coords, callback) => {
-        io.emit('newLocationMessage', generateLocationMessage("Admin", coords.latitude, coords.longitude))
+        io.emit('newLocationMessage', generateLocationMessage("Admin", coords.latitude, coords.longitude));
         callback();
     });
 
